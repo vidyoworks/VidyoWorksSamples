3d1deaf51d8e902088f97e181487e6a545aaec1a
Dinesh Sinha <Dinesh@vidyo.com>
2018-03-26 23:02:33 -0400

    Code reorg

--- Scripts/apiusage.sh
+++ Scripts/apiusage.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 #   copyright (c) 2011-2018 Vidyo, Inc. All rights reserved.
 
+function ParseUsage 
+{
+ CALLER_TYPE=$1
+ FILTER_VAL=$2
+ 
+ echo "***********************Analysing all $CALLER_TYPE***********************" >> $LOG_FILE >&1
+ echo "Unique Occurrences" >> $LOG_FILE >&1
+ grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*$FILTER_VAL\w*" $TARGET_FOLDER  | sort  | uniq  | wc -l >> $LOG_FILE >&1
+ echo "Spread across in number of files:" >> $LOG_FILE >&1
+ grep -ohir --include='*.cpp' --include='*.c' --include='*.m'  --include='*.mm' -le "\w*$FILTER_VALV\w*" $TARGET_FOLDER  | sort  | uniq | wc -l >> $LOG_FILE >&1
+ echo "*******All events ******" >> $LOG_FILE >&1
+ grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*$FILTER_VAL\w*" $TARGET_FOLDER >> $LOG_FILE >&1
+ echo "*******All $CALLER_TYPE done ******" >> $LOG_FILE >&1
+}
+
 
 # check for valid tag
 if [ -z "$1" ]; then
@@ -33,42 +48,11 @@ echo "Starting"
 
 NOWBEGIN=$(date +"%T")
 echo "Starting at $NOWBEGIN" > $LOG_FILE >&1
-echo "Analysing all events" >> $LOG_FILE >&1
-echo "Unique Occurrences" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_IN_EVENT_\w*" $TARGET_FOLDER  | sort  | uniq  | wc -l >> $LOG_FILE >&1
-echo "Spread across in number of files:" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c' --include='*.m'  --include='*.mm' -le "\w*VIDYO_CLIENT_IN_EVENT_\w*" $TARGET_FOLDER  | sort  | uniq | wc -l >> $LOG_FILE >&1
-echo "*******All events ******" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_IN_EVENT_\w*" $TARGET_FOLDER >> $LOG_FILE >&1
-echo "*******All events done ******" >> $LOG_FILE >&1
+ParseUsage "events" "VIDYO_CLIENT_IN_EVENT_"
+ParseUsage "requests" "VIDYO_CLIENT_REQUEST_"
+ParseUsage "notifications" "VIDYO_CLIENT_OUT_EVENT_"
+ParseUsage "privates" "VIDYO_CLIENT_PRIVATE_"
 
-echo "***********************Analysing all requests***********************" >> $LOG_FILE >&1
-echo "Unique Occurrences" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_REQUEST_\w*" $TARGET_FOLDER  | sort  | uniq  | wc -l >> $LOG_FILE >&1
-echo "Spread across in number of files:" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c' --include='*.m'  --include='*.mm' -le "\w*VIDYO_CLIENT_REQUEST_\w*" $TARGET_FOLDER  | sort  | uniq | wc -l >> $LOG_FILE >&1
-echo "*******All requests ******" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_REQUEST_\w*" $TARGET_FOLDER >> $LOG_FILE >&1
-echo "*******All requests done ******" >> $LOG_FILE >&1
-
-echo "***********************Analysing all notifications***********************" >> $LOG_FILE >&1
-echo "Unique Occurrences" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_OUT_EVENT_\w*" $TARGET_FOLDER  | sort  | uniq  | wc -l >> $LOG_FILE >&1
-echo "Spread across in number of files:" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c' --include='*.m'  --include='*.mm' -le "\w*VIDYO_CLIENT_OUT_EVENT_\w*" $TARGET_FOLDER  | sort  | uniq | wc -l >> $LOG_FILE >&1
-echo "*******All notifications ******" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_OUT_EVENT_\w*" $TARGET_FOLDER >> $LOG_FILE >&1
-echo "*******All notifications done ******" >> $LOG_FILE >&1
-
-
-echo "***********************Analysing all undocumented***********************" >> $LOG_FILE >&1
-echo "Unique Occurrences" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_PRIVATE_\w*" $TARGET_FOLDER  | sort  | uniq  | wc -l >> $LOG_FILE >&1
-echo "Spread across in number of files:" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c' --include='*.m' --include='*.mm' -le "\w*VIDYO_CLIENT_PRIVATE_\w*" $TARGET_FOLDER  | sort  | uniq | wc -l >> $LOG_FILE >&1
-echo "*******All undocumented ******" >> $LOG_FILE >&1
-grep -ohir --include='*.cpp' --include='*.c'  --include='*.m'  --include='*.mm' "\w*VIDYO_CLIENT_PRIVATE_\w*" $TARGET_FOLDER >> $LOG_FILE >&1
-echo "*******All undocumented done ******" >> $LOG_FILE >&1
 NOWBEGIN=$(date +"%T")
 echo "Completed at $NOWBEGIN" >> $LOG_FILE >&1
 echo "*********************************"
