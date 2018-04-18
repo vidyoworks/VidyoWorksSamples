7e69f60a022b54b2bfc0f8c63197458decf11771
Dinesh Sinha <Dinesh@vidyo.com>
2018-04-18 18:43:35 -0400

    customer profile

--- Vex/vexschedule/main.js
+++ Vex/vexschedule/main.js
@@ -22,10 +22,29 @@ function b64EncodeUnicode(str) {
     }));
 }
 
+function StringToDictionary(input) {
+    var inputArray = input.split('\n');
+    var dictOutput = [];
+    for (var i = 0; i < inputArray.length; ++i) 
+    {
+        var line = inputArray[i];
+        if(line.length == 0)
+            continue;
+        var components = line.split(':');
+        if(components.length < 2)
+            continue;
+
+        dictOutput[components[0].trim()] = components[1].trim();
+
+      }
+      console.log("....dinesh...typrof = ", (typeof dictOutput), "token = ", dictOutput["token"]);
+      return dictOutput;
+}
+
 
 formScheduleString = function()
  {
-    var scheduleToken = document.getElementById("scheduleToken").value;
+    /*var scheduleToken = document.getElementById("scheduleToken").value;
     var tenantId = document.getElementById("tenantId").value;
     var serverURL = document.getElementById("serverURL").value;
     var agentId = document.getElementById("agentId").value;
@@ -40,7 +59,7 @@ formScheduleString = function()
     var encTotal = b64EncodeUnicode(total);
     encTotal += "=";
 
-    var scheduleLink = appLocation + encTotal;
+    var scheduleLink = appLocation + encTotal;*/
 
 
 
@@ -48,8 +67,51 @@ formScheduleString = function()
 
     $.ajax({
         url: "https://vex.vidyoclouddev.com/v2/customerProfile?tenant=64",
-        //dataType: "jsonp",
+        dataType: "jsonp",
         jsonp: false,
         jsonpCallback: "localJsonpCallback"
     });*/
+
+
+    $.getJSON("https://vex.vidyoclouddev.com/v2/customerProfile?tenant=64",
+         	  {
+         	  
+         	  },
+         	  function(data) {
+                   console.log(data);
+         	  
+         	  });
+}
+
+
+
+
+myFunction = function()
+ {
+  
+
+    $.getJSON("https://vex.vidyoclouddev.com/v2/customerProfile?tenant=64",
+         	  {
+         	  
+         	  },
+         	  function(data, textStatus, xhr) {
+                   console.log(data);
+                   var webrtcURL = data.webrtcURL;
+                   console.log("....dinesh.... = ", webrtcURL);
+
+                   var headers = xhr.getAllResponseHeaders();
+                   console.log("....dinesh...tpeof = ", (typeof headers), "headers = ", headers);
+                   var dictHeader =  StringToDictionary(headers);
+                   console.log("....dinesh...dict = ", dictHeader);
+                   var test="token";
+                   console.log("....dinesh...typrof = ", (typeof dictHeader), "token = ", dictHeader[test]);
+                   for (var key in dictHeader) {
+                    // check if the property/key is defined in the object itself, not in parent
+                    if (dictHeader.hasOwnProperty(key)) {           
+                        console.log("key=", key);
+                        console.log("val=", dictHeader[key]);
+                    }
+                }
+
+         	  });
 }
