788f7995ab1b10fe9173e3a8ab2651e9e77e3e87
Dinesh Sinha <dinesh@vidyo.com>
2018-05-12 11:12:16 -0400

    Audio settings

---
+++ Android/VcAudioSettings/VcAudioSettings/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# ndk r11 is used
+# reference $NDK_ROOT/build/core/build-local.mk
+#stage 1 - armeabi-v7a
+export VIDYO_TYPE_OF_PROCESSOR=armeabi-v7a
+echo "step 1 VIDYO_TYPE_OF_PROCESSOR  = $VIDYO_TYPE_OF_PROCESSOR"
+#remove all older so files other than from where it is supposed to be in
+find obj -iname "*.so" -exec rm -R {} \;
+find app/src/main/jniLibs -iname "*.so" -exec rm -R {} \;
+rm -rf obj
+rm -rf build
+rm -rf app/build
+cp -rf app/src/main/jni/lib/$VIDYO_TYPE_OF_PROCESSOR/libVidyoClientApp.so app/src/main/jni/lib/libVidyoClientApp.so
+ndk-build NDK_PROJECT_PATH=. NDK_LIBS_OUT=./app/src/main/jniLibs NDK_APPLICATION_MK=app/src/main/jni/Application.mk APP_BUILD_SCRIPT=app/src/main/jni/Android.mk
+
+export VIDYO_TYPE_OF_PROCESSOR=arm64-v8a
+echo "step 2 VIDYO_TYPE_OF_PROCESSOR  = $VIDYO_TYPE_OF_PROCESSOR"
+#remove all older so files other than from where it is supposed to be in
+rm -rf obj
+rm -rf build
+rm -rf app/build
+cp -rf app/src/main/jni/lib/$VIDYO_TYPE_OF_PROCESSOR/libVidyoClientApp.so app/src/main/jni/lib/libVidyoClientApp.so
+ndk-build NDK_PROJECT_PATH=. NDK_LIBS_OUT=./app/src/main/jniLibs NDK_APPLICATION_MK=app/src/main/jni/Application.mk APP_BUILD_SCRIPT=app/src/main/jni/Android.mk
